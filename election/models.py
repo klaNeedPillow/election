61fc89671155720e4afd39584335d8d80301e315
klaNeedPillow <nametongla@gmail.com>
2026-03-25 20:09:40 +0700

    Register election app and create Candidate model

--- election/models.py
+++ election/models.py
@@ -1,15 +1,9 @@
 from django.db import models
-from django.contrib.auth.models import User
 
 class Candidate(models.Model):
     name = models.CharField(max_length=100)
-    description = models.TextField()
-    image = models.ImageField(upload_to='candidates/', blank=True)
+    number = models.IntegerField(unique=True) # เบอร์ผู้สมัคร
+    policy = models.TextField() # นโยบาย
 
     def __str__(self):
-        return self.name
-
-class Vote(models.Model):
-    voter = models.OneToOneField(User, on_delete=models.CASCADE) # 1 คน 1 สิทธิ์
-    candidate = models.ForeignKey(Candidate, on_delete=models.CASCADE)
-    voted_at = models.DateTimeField(auto_now_add=True)
+        return f"{self.number}: {self.name}"
