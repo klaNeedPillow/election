a23c83c32dea87b2f32b48c944e4402d1d08f486
klaNeedPillow <nametongla@gmail.com>
2026-03-25 20:18:24 +0700

    Create election/urls.py and link to project urls

---
+++ election/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('vote/<int:candidate_id>/', views.cast_vote, name='vote'),
+]
--- election/views.py
+++ election/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Candidate, Vote
+from django.db.models import Count
 
-# Create your views here.
+def index(request):
+    
+    if not request.session.session_key:
+        request.session.create()
+    
+    session_key = request.session.session_key
+    candidates = Candidate.objects.all()
+    
+    
+    has_voted = Vote.objects.filter(session_id=session_key).exists()
+   
+    results = Candidate.objects.annotate(total_votes=Count('vote'))
+    
+    context = {
+        'candidates': candidates,
+        'has_voted': has_voted,
+        'results': results
+    }
+    return render(request, 'election/index.html', context)
+
+def cast_vote(request, candidate_id):
+    session_key = request.session.session_key
+    candidate = Candidate.objects.get(id=candidate_id)
+   
+    if not Vote.objects.filter(session_id=session_key).exists():
+        Vote.objects.create(candidate=candidate, session_id=session_key)
+        
+    return redirect('index')
